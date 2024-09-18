174857688fe0194b563a36115bebb3be64e4cfbe
gastonMacias <gastonezequielmacias@gmail.com>
2024-09-18 19:48:43 -0300

    inicio de componente

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import "./header.css";
+
+
+const Header = () => {
+    return (
+        <header>
+            <h1>FOODMAX</h1>
+            <p>Tu lugar de recetas!!</p>
+        </header>
+    )
+}
+
+
+export default Header;
