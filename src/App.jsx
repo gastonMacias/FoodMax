9e50510a80043a10f9d354ab3506c5f5726c5186
gastonMacias <gastonezequielmacias@gmail.com>
2024-09-24 11:08:57 -0300

    se borro la imagen logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import Navbar from "../src/components/navbar/Navbar";
 import './App.css'
 
 function App() {
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      
+      <Navbar />
     </>
   )
 }
