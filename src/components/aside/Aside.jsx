ff289e8e891529c697d3bb005ae3b715782c6db3
gastonMacias <gastonezequielmacias@gmail.com>
2024-09-26 15:21:45 -0300

    componente aside primeros pasos

--- src/components/aside/Aside.jsx
+++ src/components/aside/Aside.jsx
@@ -1,10 +1,24 @@
+import "./aside.css";
 
 
 
 const Aside = () => {
-    return <section>
-
-    </section>
+    return <aside className="aside">
+        <div>
+            <h4>Buscador</h4>
+            <input type="text" placeholder="busqueda"/>
+        </div>
+        <div>
+            <h4>Secciones</h4>
+            <ul>
+                <li>pastas</li>
+                <li>carnes</li>
+                <li>postres</li>
+                <li>vegana</li>
+                <li>vegetariana</li>
+            </ul>
+        </div>
+    </aside>
 }
 
 
