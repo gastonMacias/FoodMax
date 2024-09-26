6fe5b692597c9e9ef3d8a49106a021041e2d7d39
gastonMacias <gastonezequielmacias@gmail.com>
2024-09-26 12:25:00 -0300

    creacion de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+//secciones
 import Navbar from "../src/components/navbar/Navbar";
+
+// pages
+import Home from "../src/pages/home/Home";
+import Favoritas from "./pages/favoritas/Favoritas";
+import MiRecetario from "../src/pages/miRecetario/MiRecetario";
+
 import './App.css'
 
 function App() {
-  
+
 
   return (
     <>
       <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/favoritas" element={<Favoritas />} />
+          <Route path="/miRecetario" element={<MiRecetario />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
