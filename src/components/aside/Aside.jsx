aee67841403c35e699ab3d92a9f90be6d338cda3
gastonMacias <gastonezequielmacias@gmail.com>
2024-10-02 14:33:21 -0300

    componente por el momento en standby

--- src/components/aside/Aside.jsx
+++ src/components/aside/Aside.jsx
@@ -1,23 +1,12 @@
+import { Box } from "@mui/material";
 import "./aside.css";
 
 
 
 const Aside = () => {
     return <aside className="aside">
-        <div>
-            <h4>Buscador</h4>
-            <input type="text" placeholder="busqueda"/>
-        </div>
-        <div>
-            <h4>Secciones</h4>
-            <ul>
-                <li>pastas</li>
-                <li>carnes</li>
-                <li>postres</li>
-                <li>vegana</li>
-                <li>vegetariana</li>
-            </ul>
-        </div>
+        
+
     </aside>
 }
 
