fed79d1a3b1e4457741b0c2d4ac96065209f7375
gastonMacias <gastonezequielmacias@gmail.com>
2024-10-02 15:05:53 -0300

    estilos dados con material ui se agrego icono favoritos y boton de receta (en principio abriria un modal)

--- src/components/tarjeta/Tarjeta.jsx
+++ src/components/tarjeta/Tarjeta.jsx
@@ -1,26 +1,30 @@
 import Card from "@mui/material/Card";
 import "./tarjeta.css";
-import { Button, CardActionArea, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
+import { Button, CardActionArea, CardActions, CardContent, CardMedia, IconButton, Typography } from "@mui/material";
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
 
-  const Tarjeta = ({ imagen, nombre, comentario }) => {
+const Tarjeta = ({ imagen, nombre, comentario }) => {
     return (
-        <Card sx={{ maxWidth: 245 }}>
+        <Card sx={{ maxWidth: 190, maxHeight: 250 }}>
             <CardActionArea>
                 <CardMedia component="img" height="140" image={imagen} alt={nombre} />
                 <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
+                    <Typography gutterBottom variant="h7" component="div" textAlign={"center"} fontWeight={"bold"}>
                         {nombre}
                     </Typography>
-                    {/* <Typography variant="body2" sx={{ color: "InfoText", fontSize: 10 }}>
-                        {ingredients.join(', ')}
-                    </Typography> */}
+                    <Typography variant="body2" sx={{ color: "InfoText", fontSize: 10 }}>
+                        {comentario}
+                    </Typography>
                 </CardContent>
             </CardActionArea>
-            <CardActions>
-                <Button size="small" color="primary">
-                    Share
+            <CardActions sx={{ justifyContent: 'center', alignItems: 'center' }}>
+                <Button size="small" color="success">
+                    Receta
                 </Button>
+                <IconButton color="success">
+                    <FavoriteBorderIcon />
+                </IconButton>
             </CardActions>
         </Card>
     );
