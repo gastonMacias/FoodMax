268606ebd0040be55746c08b8910faf0cd02cc57
gastonMacias <gastonezequielmacias@gmail.com>
2024-10-03 13:16:20 -0300

    inclusion del modal

--- src/components/tarjeta/Tarjeta.jsx
+++ src/components/tarjeta/Tarjeta.jsx
@@ -2,9 +2,17 @@ import Card from "@mui/material/Card";
 import "./tarjeta.css";
 import { Button, CardActionArea, CardActions, CardContent, CardMedia, IconButton, Typography } from "@mui/material";
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import { useState } from "react";
+import ModalReceta from "../modalReceta/ModalReceta";
+
 
 
 const Tarjeta = ({ imagen, nombre, comentario }) => {
+    const [open, setOpen] = useState(false);
+
+    const handleClose = () => setOpen(false);
+    const handleOpen = () => setOpen(true);
+    
     return (
         <Card sx={{ maxWidth: 190, maxHeight: 250 }}>
             <CardActionArea>
@@ -19,13 +27,15 @@ const Tarjeta = ({ imagen, nombre, comentario }) => {
                 </CardContent>
             </CardActionArea>
             <CardActions sx={{ justifyContent: 'center', alignItems: 'center' }}>
-                <Button size="small" color="success">
+                <Button size="small" color="success" onClick={handleOpen}>
                     Receta
                 </Button>
                 <IconButton color="success">
                     <FavoriteBorderIcon />
                 </IconButton>
             </CardActions>
+
+            <ModalReceta open={open} handleClose={handleClose} />
         </Card>
     );
 }
