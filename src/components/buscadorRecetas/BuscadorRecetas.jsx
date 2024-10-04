3ba9cd90e603d39960fa29012d1a1bdbf98a2070
gastonMacias <gastonezequielmacias@gmail.com>
2024-10-04 14:33:44 -0300

    componente buscador de recetas falta logica de buscador

---
+++ src/components/buscadorRecetas/BuscadorRecetas.jsx
@@ -0,0 +1,13 @@
+import "./buscadorRecetas.css";
+
+const BuscadorRecetas = () => {
+    return <div className="buscador">
+        <h3>BUSCADOR DE RECETAS</h3>
+
+        <div className="buscador_input">
+            <input type="text" placeholder="Que te gustaria degustar?"/>
+        </div>
+    </div>
+}
+
+export default BuscadorRecetas;
