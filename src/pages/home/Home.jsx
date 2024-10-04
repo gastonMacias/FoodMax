936ea992e673506087a1998af9d7f084886611ee
gastonMacias <gastonezequielmacias@gmail.com>
2024-10-04 14:34:18 -0300

    se implemento componente buscador

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,13 +2,14 @@ import "./home.css";
 
 
 import SeccionHome from "../../components/seccionHome/SeccionHome";
-import Aside from "../../components/aside/Aside"
+import BuscadorRecetas from "../../components/buscadorRecetas/BuscadorRecetas";
+// import Aside from "../../components/aside/Aside"
 
 
 const Home = () => {
     return (<>
         <div className="home">
-            <Aside />
+            <BuscadorRecetas />
             <SeccionHome />
         </div>
     </>
