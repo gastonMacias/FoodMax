41266ad1fb08abc1de583137c7661b799650b823
gastonMacias <gastonezequielmacias@gmail.com>
2024-10-04 13:51:28 -0300

    se cambio la propiedad base para que funcione correctamente en modo local como gh pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://gastonMacias.github.io/FoodMax"
+  base: mode === 'production' ? 'https://gastonMacias.github.io/FoodMax' : '/',
 })
+
+
+// mode === 'production': Este código se asegura de que la propiedad base solo se aplique cuando ejecutes 
+// npm run build, que es el comando que se usa para desplegar en producción (como GitHub Pages). 
+// En desarrollo (npm run dev), base será '/', que es lo correcto para el entorno local.
