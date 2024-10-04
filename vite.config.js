7d5ddce9e38d4088056e5f430f09d0c766afde66
gastonMacias <gastonezequielmacias@gmail.com>
2024-10-04 14:41:44 -0300

    correccion para npm run dev

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: mode === 'production' ? 'https://gastonMacias.github.io/FoodMax' : '/',
-})
-
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === 'production' ? '/FoodMax/' : '/',
+  };
+});
 
 // mode === 'production': Este código se asegura de que la propiedad base solo se aplique cuando ejecutes 
 // npm run build, que es el comando que se usa para desplegar en producción (como GitHub Pages). 
