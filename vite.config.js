a410c9e7c37e37b051db5bf8519bffb9faedf727
gastonMacias <gastonezequielmacias@gmail.com>
2025-07-29 19:18:21 -0300

    optimizados para desplegar en vercel

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  return {
-    plugins: [react()],
-    base: mode === 'production' ? '/FoodMax/' : '/',
-  };
+export default defineConfig({
+  plugins: [react()],
 });
 
-// mode === 'production': Este código se asegura de que la propiedad base solo se aplique cuando ejecutes 
-// npm run build, que es el comando que se usa para desplegar en producción (como GitHub Pages). 
-// En desarrollo (npm run dev), base será '/', que es lo correcto para el entorno local.
