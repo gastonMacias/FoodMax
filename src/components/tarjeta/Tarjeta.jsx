857f25ab9bd80f5d1bd9ea6b9ec96f863269e0a1
gastonMacias <gastonezequielmacias@gmail.com>
2025-07-30 18:14:44 -0300

    se genero una nueva card con primeros estilos

--- src/components/tarjeta/Tarjeta.jsx
+++ src/components/tarjeta/Tarjeta.jsx
@@ -7,42 +7,55 @@ import ModalReceta from "../modalReceta/ModalReceta";
 
 
 
-const Tarjeta = ({ imagen, nombre, comentario }) => {
+const Tarjeta = ({ imagen, nombre }) => {
     const [open, setOpen] = useState(false);
 
     const handleClose = () => setOpen(false);
     const handleOpen = () => setOpen(true);
 
-    const imageUrl = process.env.NODE_ENV === 'development'
-        ? `/public${imagen}`  // Agregamos '/public' en local
-        : `${import.meta.env.BASE_URL}${imagen}`;  // En producción
+    
 
     return (
-        <Card sx={{ maxWidth: 190, maxHeight: 250 }}>
-            <CardActionArea>
-                <CardMedia component="img" height="140" image={imageUrl} alt={nombre} />
-                <CardContent>
-                    <Typography gutterBottom variant="h7" component="div" textAlign={"center"} fontWeight={"bold"}>
-                        {nombre}
-                    </Typography>
-                    <Typography variant="body2" sx={{ color: "InfoText", fontSize: 10 }}>
-                        {comentario}
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
-            <CardActions sx={{ justifyContent: 'center', alignItems: 'center' }}>
-                <Button size="small" color="success" onClick={handleOpen}>
-                    Receta
-                </Button>
-                <IconButton color="success">
-                    <FavoriteBorderIcon />
-                </IconButton>
-            </CardActions>
-
-            <ModalReceta open={open} handleClose={handleClose} />
-        </Card>
+        <>
+            <div className="container__card">
+                <figure className="figure__card">
+                    <img src={imagen} alt={nombre} className="imagen__card" />
+                    <figcaption className="titulo__card">{nombre}</figcaption>
+                </figure>
+                
+                <div className="action__card">
+                    <button className="button__card">Receta</button>
+                    <FavoriteBorderIcon className="icon__card" />
+                </div>
+            </div>
+        </>
     );
 }
 
 
 export default Tarjeta;
+
+
+// <Card sx={{ maxWidth: 190, maxHeight: 250 }}>
+//     <CardActionArea>
+//         <CardMedia component="img" height="140" image={imageUrl} alt={nombre} />
+//         <CardContent>
+//             <Typography gutterBottom variant="h7" component="div" textAlign={"center"} fontWeight={"bold"}>
+//                 {nombre}
+//             </Typography>
+//             <Typography variant="body2" sx={{ color: "InfoText", fontSize: 10 }}>
+//                 {comentario}
+//             </Typography>
+//         </CardContent>
+//     </CardActionArea>
+//     <CardActions sx={{ justifyContent: 'center', alignItems: 'center' }}>
+//         <Button size="small" color="success" onClick={handleOpen}>
+//             Receta
+//         </Button>
+//         <IconButton color="success">
+//             <FavoriteBorderIcon />
+//         </IconButton>
+//     </CardActions>
+
+//     <ModalReceta open={open} handleClose={handleClose} />
+// </Card>
