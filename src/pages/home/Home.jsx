4209cf3f0c29680df6cc564c5307fe83d0b57b47
gastonMacias <gastonezequielmacias@gmail.com>
2025-07-30 18:15:06 -0300

    se implemento el componente carrousel

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,14 @@ import "./home.css";
 
 
 import SeccionHome from "../../components/seccionHome/SeccionHome";
+import Carrousel from "../../components/carrousel/Carrousel";
 
 
 
 const Home = () => {
     return (<>
         <div className="home">
+            <Carrousel />
             <SeccionHome />
         </div>
     </>
